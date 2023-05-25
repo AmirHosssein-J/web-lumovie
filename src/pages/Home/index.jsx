ab406e3e0eb2ecd3d1b5c8021866dfcc2065d625
AmirHossein <amirhosssein.jfr@gmail.com>
2023-05-26 01:40:16 +0330

    featured in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { BASE_URL, API_KEY } from "/src/api";
+
+import useFetch from "/src/hooks/useFetch";
+
+import ErrorText from "/src/pages/Error/ErrorText";
+import OnLoading from "/src/components/Posters/OnLoading";
+import Posters from "/src/components/Posters";
+
 import S from "./home.module.scss";
 import MediaQuery, { useMediaQuery } from "react-responsive";
 
@@ -13,6 +22,17 @@ import Popular from "./Popular";
 const Home = () => {
   const onTabletOrMobile = useMediaQuery({ query: "(max-width: 61.875rem)" });
 
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["featured"],
+    queryFn: () =>
+      useFetch(
+        `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&region=US&page=2`
+      ),
+  });
+
+  if (isLoading) return <OnLoading />;
+  if (isError) return <ErrorText message={error.message} />;
+
   return (
     <div className={S["home"]}>
       <section
@@ -20,6 +40,7 @@ const Home = () => {
           S["content"]
         }`}
       >
+        <Posters data={data.results} />
         {/* <Featured /> */}
         <NewRelease />
 
