bea4a94911691289c33dbda2472be9e08a6dc1c5
AmirHossein <amirhosssein.jfr@gmail.com>
2023-05-26 01:42:51 +0330

    changed featured file

---
+++ src/pages/Home/FeaturedMovies/index.jsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { BASE_URL, API_KEY } from "/src/api";
+
+import useFetch from "/src/hooks/useFetch";
+
+import ErrorText from "/src/pages/Error/ErrorText";
+import OnLoading from "/src/components/Posters/OnLoading";
+import Posters from "/src/components/Posters";
+
+const FeaturedMovies = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["featured"],
+    queryFn: () =>
+      useFetch(
+        `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&region=US&page=2`
+      ),
+  });
+
+  if (isLoading) return <OnLoading />;
+  if (isError) return <ErrorText message={error.message} />;
+
+  return <Posters data={data.results} />;
+};
+
+export default FeaturedMovies;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
-import { BASE_URL, API_KEY } from "/src/api";
-
-import useFetch from "/src/hooks/useFetch";
-
-import ErrorText from "/src/pages/Error/ErrorText";
-import OnLoading from "/src/components/Posters/OnLoading";
-import Posters from "/src/components/Posters";
-
 import S from "./home.module.scss";
 import MediaQuery, { useMediaQuery } from "react-responsive";
 
@@ -15,23 +6,12 @@ import Cinema from "/src/components/Cards/Cinema";
 import Upcomings from "/src/components/Cards/Upcomings";
 import SerieSideBar from "/src/layout/SideBars/SerieSideBar";
 
-// import Featured from "./Featured";
 import NewRelease from "./NewRelease";
 import Popular from "./Popular";
+import FeaturedMovies from "./FeaturedMovies";
 
 const Home = () => {
   const onTabletOrMobile = useMediaQuery({ query: "(max-width: 61.875rem)" });
-
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["featured"],
-    queryFn: () =>
-      useFetch(
-        `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&region=US&page=2`
-      ),
-  });
-
-  if (isLoading) return <OnLoading />;
-  if (isError) return <ErrorText message={error.message} />;
 
   return (
     <div className={S["home"]}>
@@ -40,8 +20,7 @@ const Home = () => {
           S["content"]
         }`}
       >
-        <Posters data={data.results} />
-        {/* <Featured /> */}
+        <FeaturedMovies />
         <NewRelease />
 
         <Upcomings />
