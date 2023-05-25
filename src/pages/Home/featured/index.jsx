51f40137a369bd167d03f1dee2c24c61e34e6a4c
AmirHossein <amirhosssein.jfr@gmail.com>
2023-05-26 01:34:04 +0330

    featured testing

--- src/pages/Home/featured/index.jsx
+++ src/pages/Home/featured/index.jsx
@@ -8,18 +8,23 @@ import OnLoading from "/src/components/Posters/OnLoading";
 import Posters from "/src/components/Posters";
 
 const Featured = () => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["featured"],
-    queryFn: () =>
-      useFetch(
-        `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&region=US&page=2`
-      ),
-  });
+  // const { data, isLoading, isError, error } = useQuery({
+  //   queryKey: ["featured"],
+  //   queryFn: () =>
+  //     useFetch(
+  //       `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&region=US&page=2`
+  //     ),
+  // });
 
-  if (isLoading) return <OnLoading />;
-  if (isError) return <ErrorText message={error.message} />;
+  // if (isLoading) return <OnLoading />;
+  // if (isError) return <ErrorText message={error.message} />;
 
-  return <>{/* <Posters data={data.results} /> */}</>;
+  return (
+    <>
+      <h1>hello</h1>
+      {/* <Posters data={data.results} /> */}
+    </>
+  );
 };
 
 export default Featured;
